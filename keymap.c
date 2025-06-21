602027069232b7166bc0d55757ae719fc11a60aa
MateuszMielniczuk <matheuszmmi@gmail.com>
2025-06-21 22:38:58 +0000

    Remove shift from bottom corners fix brackets

--- keymap.c
+++ keymap.c
@@ -73,7 +73,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_QWERTY] = LAYOUT(
      KC_TAB  , KC_Q ,  KC_W   ,  KC_E  ,   KC_R ,   KC_T ,                                        KC_Y,   KC_U ,  KC_I ,   KC_O ,  KC_P , KC_BSPC,
      CTL_ESC , KC_A ,  KC_S   ,  KC_D  ,   KC_F ,   KC_G ,                                        KC_H,   KC_J ,  KC_K ,   KC_L ,KC_SCLN,CTL_QUOT,
-     KC_LSFT , KC_Z ,  KC_X   ,  KC_C  ,   KC_V ,   KC_B , KC_LBRC,KC_CAPS,     FKEYS  , KC_RBRC, KC_N,   KC_M ,KC_COMM, KC_DOT ,KC_SLSH, KC_RSFT,
+      _______, KC_Z ,  KC_X   ,  KC_C  ,   KC_V ,   KC_B , KC_LBRC,KC_CAPS,     FKEYS  , KC_RBRC, KC_N,   KC_M ,KC_COMM, KC_DOT ,KC_SLSH, _______,
                                 ADJUST , KC_LGUI, KC_LALT, KC_SPC , NAV   ,     SYM    , KC_ENT ,KC_RALT, KC_RGUI, KC_APP
     ),
 
@@ -85,7 +85,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+------|                              |------+------+------+------+------+--------|
  * |Ctrl/Esc|   A  |   O  |SHFT/E|   U  |   I  |                              |   D  |   H  |SHFT/T|   N  |   L  |Ctrl/-_ |
  * |--------+------+------+------+------+------+-------------.  ,-------------+------+------+------+------+------+--------|
- * | LShift | ; :  |   Q  |   J  |   K  |   X  | [ {  |CapsLk|  |F-keys|  ] } |   B  |   M  |   W  |   V  |   Z  | RShift |
+ * |        | ; :  |   Q  |   J  |   K  |   X  | [ {  |CapsLk|  |F-keys|  ] } |   B  |   M  |   W  |   V  |   Z  |        |
  * `----------------------+------+------+------+------+------|  |------+------+------+------+------+----------------------'
  *                        (Adjust) LGUI | LAlt | Space| Nav  |  | Sym  | Enter| AltGr| RGUI ( Menu )
  *                        `----------------------------------'  `----------------------------------'
@@ -257,18 +257,19 @@ bool oled_task_user(void) {
     if (is_keyboard_master()) {
         // QMK Logo and version information
         // clang-format off
-        // static const char PROGMEM qmk_logo[] = {
-        //     0x80,0x81,0x82,0x83,0x84,0x85,0x86,0x87,0x88,0x89,0x8a,0x8b,0x8c,0x8d,0x8e,0x8f,0x90,0x91,0x92,0x93,0x94,
-        //     0xa0,0xa1,0xa2,0xa3,0xa4,0xa5,0xa6,0xa7,0xa8,0xa9,0xaa,0xab,0xac,0xad,0xae,0xaf,0xb0,0xb1,0xb2,0xb3,0xb4,
-        //     0xc0,0xc1,0xc2,0xc3,0xc4,0xc5,0xc6,0xc7,0xc8,0xc9,0xca,0xcb,0xcc,0xcd,0xce,0xcf,0xd0,0xd1,0xd2,0xd3,0xd4,0};
+        static const char PROGMEM qmk_logo[] = {
+            0x80,0x81,0x82,0x83,0x84,0x85,0x86,0x87,0x88,0x89,0x8a,0x8b,0x8c,0x8d,0x8e,0x8f,0x90,0x91,0x92,0x93,0x94,
+            0xa0,0xa1,0xa2,0xa3,0xa4,0xa5,0xa6,0xa7,0xa8,0xa9,0xaa,0xab,0xac,0xad,0xae,0xaf,0xb0,0xb1,0xb2,0xb3,0xb4,
+            0xc0,0xc1,0xc2,0xc3,0xc4,0xc5,0xc6,0xc7,0xc8,0xc9,0xca,0xcb,0xcc,0xcd,0xce,0xcf,0xd0,0xd1,0xd2,0xd3,0xd4,0
+    };
         // clang-format on
 
-        // oled_write_P(qmk_logo, false);
-        oled_write_P(PSTR("     craVtec\n\n"), false);
+        oled_write_P(qmk_logo, false);
         oled_write_P(PSTR("Kyria rev3.0\n\n"), false);
 
         // Host Keyboard Layer Status
-        oled_write_P(PSTR("Layer: "), false);
+
+       oled_write_P(PSTR("Layer: "), false);
         switch (get_highest_layer(layer_state|default_layer_state)) {
             case _QWERTY:
                 oled_write_P(PSTR("QWERTY\n"), false);
@@ -277,8 +278,8 @@ bool oled_task_user(void) {
             //     oled_write_P(PSTR("HSNT\n"), false);
             //     break;
             case _DVORAK:
-                oled_write_P(PSTR("Dvorak\n"), false);
-                break;
+               oled_write_P(PSTR("Dvorak\n"), false);
+               break;
             case _COLEMAK_DH:
                 oled_write_P(PSTR("Colemak-DH\n"), false);
                 break;
@@ -287,13 +288,13 @@ bool oled_task_user(void) {
                 break;
             case _SYM:
                 oled_write_P(PSTR("Sym\n"), false);
-                break;
+               break;
             case _FUNCTION:
                 oled_write_P(PSTR("Function\n"), false);
                 break;
             case _ADJUST:
                 oled_write_P(PSTR("Adjust\n"), false);
-                break;
+               break;
             default:
                 oled_write_P(PSTR("Undefined\n"), false);
         }
@@ -301,7 +302,8 @@ bool oled_task_user(void) {
         // Write host Keyboard LED Status to OLEDs
         led_t led_usb_state = host_keyboard_led_state();
         oled_write_P(led_usb_state.num_lock    ? PSTR("NUMLCK ") : PSTR("       "), false);
-        oled_write_P(led_usb_state.caps_lock   ? PSTR("CAPLCK ") : PSTR("       "), false);
+
+       oled_write_P(led_usb_state.caps_lock   ? PSTR("CAPLCK ") : PSTR("       "), false);
         oled_write_P(led_usb_state.scroll_lock ? PSTR("SCRLCK ") : PSTR("       "), false);
     } else {
         // clang-format off
@@ -317,7 +319,8 @@ bool oled_task_user(void) {
          // };
         // clang-format on
         // oled_write_raw_P(kyria_logo, sizeof(kyria_logo));
-        
+
+    // Logo geenrated using website: https://javl.github.io/image2cpp/
 		static const char PROGMEM ap_logo[] = {
 		 	// 'AP', 128x64px
 		 0x00, 0x00, 0x00, 0x00, 0x04, 0x04, 0x0c, 0x0c, 0x0c, 0x0c, 0x1c, 0x1c, 0x38, 0x38, 0x38, 0x38, 
@@ -478,16 +481,16 @@ const uint16_t PROGMEM combo_close_window[] = {KC_B, KC_Z, COMBO_END};
 
 combo_t key_combos[] = {
     // left brackets
-    COMBO(combo_left_paranthesis, KC_LBRC),
-    COMBO(combo_left_bracket, KC_LPRN),
     COMBO(combo_left_curly_bracket, KC_LCBR),
+    COMBO(combo_left_bracket, KC_LBRC),
+    COMBO(combo_left_paranthesis, KC_LPRN),
     // left symbols
     COMBO(combo_pipe, KC_PIPE),
     COMBO(combo_plus, KC_PLUS),
     // right brackets
-    COMBO(combo_right_paranthesis, KC_RBRC),
-    COMBO(combo_right_bracket, KC_RPRN),
     COMBO(combo_right_curly_bracket, KC_RCBR),
+    COMBO(combo_right_bracket, KC_RBRC),
+    COMBO(combo_right_paranthesis, KC_RPRN),
     // right symbols
     COMBO(combo_equal, KC_EQL),
     COMBO(combo_minus, KC_MINS),
